a8a414ac277a885432c02cba99cd78bc668d4bca
BernickoRphl <120437917+BernickoRphl@users.noreply.github.com>
2023-12-26 17:32:42 +0700

    p

--- database/seeders/ChapterSeeder.php
+++ database/seeders/ChapterSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ChapterSeeder extends Seeder
 {
@@ -12,6 +13,30 @@ class ChapterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('chapters')->insert([
+            'title' => 'Chapter 1',
+            'description'  => 'Chapter 1 Desc',
+            'total_point' => 100,
+            'status' => true,
+        ]);
+        DB::table('chapters')->insert([
+            'title' => 'Chapter 2',
+            'description'  => 'Chapter 2 Desc',
+            'total_point' => 100,
+            'status' => true,
+        ]);
+        DB::table('chapters')->insert([
+            'title' => 'Chapter 3',
+            'description'  => 'Chapter 3 Desc',
+            'total_point' => 100,
+            'status' => true,
+        ]);
+        DB::table('chapters')->insert([
+            'title' => 'Chapter 4',
+            'description'  => 'Chapter 4 Desc',
+            'total_point' => 100,
+            'status' => true,
+        ]);
+
     }
 }
--- database/seeders/LevelSeeder.php
+++ database/seeders/LevelSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class LevelSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class LevelSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('levels')->insert([
+            'difficulty' => 'Normal',
+        ]);
+        DB::table('levels')->insert([
+            'difficulty' => 'Hard',
+        ]);
     }
 }
