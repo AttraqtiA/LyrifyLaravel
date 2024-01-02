77c54e8da914c22c88565084b62a18dfefde5e48
BernickoRphl <120437917+BernickoRphl@users.noreply.github.com>
2024-01-02 20:43:33 +0700

    Update MusicSeeder.php

--- database/seeders/MusicSeeder.php
+++ database/seeders/MusicSeeder.php
@@ -15,7 +15,7 @@ class MusicSeeder extends Seeder
     {
         DB::table('music')->insert([
             'challenge_type' => 'Type 1',
-            'title' => 'Sample Title 1',
+            'title' => 'Fix You - Coldplay',
             'genre' => 'Pop',
             'lyric' => 'Sample lyrics for song 1',
             'youtube_link' => 'https://youtu.be/AQl5nFnZPwk?si=CFEC5Px4H7XkKFDR',
@@ -28,10 +28,10 @@ class MusicSeeder extends Seeder
         ]);
         DB::table('music')->insert([
             'challenge_type' => 'Type 2',
-            'title' => 'Sample Title 2',
+            'title' => 'Fix You - Coldplay',
             'genre' => 'Pop',
             'lyric' => 'Sample lyrics for song 2',
-            'youtube_link' => 'https://youtu.be/vxQtby0s7iI?si=-JYaZ7k0QmYY89RH',
+            'youtube_link' => 'https://youtu.be/vxQtby0s7iI',
             'artist' => 'Sample Artist 2',
             'year_released' => '2022-02-01',
             'guess_duration' => 0,
@@ -41,8 +41,8 @@ class MusicSeeder extends Seeder
         ]);
         DB::table('music')->insert([
             'challenge_type' => 'Type 3',
-            'title' => 'Sample Title 3',
-            'genre' => 'Rock',
+            'title' => 'Fix You - Coldplay',
+            'genre' => 'Pop',
             'lyric' => 'Sample lyrics for song 3',
             'youtube_link' => 'https://youtu.be/W891qcD-Da0?si=vlFo45GJPLwGZlAN',
             'artist' => 'Sample Artist 3',
@@ -54,10 +54,10 @@ class MusicSeeder extends Seeder
         ]);
         DB::table('music')->insert([
             'challenge_type' => 'Type 4',
-            'title' => 'Sample Title 4',
-            'genre' => 'Rock',
+            'title' => 'Love Story - Taylor Swift',
+            'genre' => 'Pop',
             'lyric' => 'Sample lyrics for song 4',
-            'youtube_link' => 'https://youtu.be/QCmiwiSVYXI?si=aDLwYMvzcH5GL6F9',
+            'youtube_link' => 'https://youtu.be/ep1U4gtuZTU',
             'artist' => 'Sample Artist 4',
             'year_released' => '2022-04-01',
             'guess_duration' => 0,
@@ -67,10 +67,10 @@ class MusicSeeder extends Seeder
         ]);
         DB::table('music')->insert([
             'challenge_type' => 'Type 5',
-            'title' => 'Sample Title 5',
-            'genre' => 'Country',
+            'title' => 'Love Story - Taylor Swift',
+            'genre' => 'Pop',
             'lyric' => 'Sample lyrics for song 5',
-            'youtube_link' => 'https://youtu.be/2b3EzAqv_vM?si=aZPbIv9T806fmP3h',
+            'youtube_link' => 'https://youtu.be/43XEBGGHkmk?si=1uK5F0Qci3AFqyRG',
             'artist' => 'Sample Artist 5',
             'year_released' => '2022-05-01',
             'guess_duration' => 0,
@@ -80,10 +80,49 @@ class MusicSeeder extends Seeder
         ]);
         DB::table('music')->insert([
             'challenge_type' => 'Type 6',
-            'title' => 'Sample Title 6',
-            'genre' => 'Country',
+            'title' => 'Love Story - Taylor Swift',
+            'genre' => 'Pop',
             'lyric' => 'Sample lyrics for song 6',
-            'youtube_link' => 'https://youtu.be/3NIWWXfJ33A?si=0PRwI9tsvFjvmkUz',
+            'youtube_link' => 'https://youtu.be/F3pKUxrk5A4?si=bLNvkQbUjQp57nd0',
+            'artist' => 'Sample Artist 6',
+            'year_released' => '2022-05-01',
+            'guess_duration' => 0,
+            'status' => false,
+            'point' => 0,
+            'image' => 'picture_3',
+        ]);
+        DB::table('music')->insert([
+            'challenge_type' => 'Type 7',
+            'title' => 'Y2K, bbno$ - Lalala',
+            'genre' => 'Hip-Hop',
+            'lyric' => 'Sample lyrics for song 4',
+            'youtube_link' => 'https://youtu.be/QCmiwiSVYXI?si=ZWiMpRGJz5Hc78kA',
+            'artist' => 'Sample Artist 4',
+            'year_released' => '2022-04-01',
+            'guess_duration' => 0,
+            'status' => true,
+            'point' => 0,
+            'image' => 'picture_2',
+        ]);
+        DB::table('music')->insert([
+            'challenge_type' => 'Type 8',
+            'title' => 'Y2K, bbno$ - Lalala',
+            'genre' => 'Hip-Hop',
+            'lyric' => 'Sample lyrics for song 5',
+            'youtube_link' => 'https://youtu.be/2b3EzAqv_vM?si=keXtiYYv6mim5e98',
+            'artist' => 'Sample Artist 5',
+            'year_released' => '2022-05-01',
+            'guess_duration' => 0,
+            'status' => false,
+            'point' => 0,
+            'image' => 'picture_3',
+        ]);
+        DB::table('music')->insert([
+            'challenge_type' => 'Type 9',
+            'title' => 'Y2K, bbno$ - Lalala',
+            'genre' => 'Hip-Hop',
+            'lyric' => 'Sample lyrics for song 6',
+            'youtube_link' => 'https://youtu.be/3NIWWXfJ33A?si=FZKt2A_hxIR5Eb1_',
             'artist' => 'Sample Artist 6',
             'year_released' => '2022-05-01',
             'guess_duration' => 0,
